b2d3537fbbeff6d02e72430e9abf3a092c323dc4
Hassan Abbas <habbas99@gmail.com>
2016-10-16 00:07:36 -0400

    - initial test cases

---
+++ tests/markup-test.js
@@ -0,0 +1,45 @@
+/**
+ * Test cases
+ *
+ */
+
+var chai = require('chai');
+var expect = chai.expect;
+
+
+describe('Markup error cases', function() {
+	it('No amount specified', function() {
+  	});
+
+	it('Amount must be a number', function() {
+  	});
+
+  	it('Required workers must be a number', function() {
+  	});
+});
+
+describe('Markup success cases', function() {
+	it('FLAT markup on 100 should return 105', function() {
+  	});
+
+	it('One WORKER markup on 100 should return 101.2', function() {
+  	});
+
+  	it('Five WORKER markup on 100 should return 106', function() {
+  	});
+	
+	it('ELECTRONIC markup on 100 should return 102', function() {
+  	});
+
+	it('PHARMACEUTICAL markup on 100 should return 107.5', function() {
+  	});
+
+  	it('FOOD markup on 100 should return 113', function() {
+  	});
+
+  	it('food (lower-cased) markup on 100 should return 113', function() {
+  	});
+
+  	it('ALL OTHER TYPES markup on 100 should return 100', function() {
+  	});
+});
