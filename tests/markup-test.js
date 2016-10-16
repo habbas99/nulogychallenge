0160f7d7f4b231e6a5ff6bdf5a497d923d3216b8
Hassan Abbas <habbas99@gmail.com>
2016-10-16 12:56:07 -0400

    - tests markup modified and final cost estimate added

--- tests/markup-test.js
+++ tests/markup-test.js
@@ -7,42 +7,59 @@ var chai = require('chai');
 var expect = chai.expect;
 
 
-describe('Markup error cases', function() {
+describe('Markup - error cases', function() {
 	it('No amount specified', function() {
   	});
 
-	it('Amount must be a number', function() {
+	it('Amount must be a string or a number', function() {
   	});
 
   	it('Required workers must be a number', function() {
   	});
 });
 
-describe('Markup success cases', function() {
-	it('FLAT markup on 100 should return 105', function() {
+describe('Markup - success cases', function() {
+	it('FLAT markup on 100 should return 5', function() {
   	});
 
-	it('One WORKER markup on 100 should return 101.2', function() {
+	it('One WORKER markup on 100 should return 1.2', function() {
   	});
 
-  	it('Five WORKER markup on 100 should return 106', function() {
+  	it('Five WORKER markup on 100 should return 6', function() {
   	});
 	
-	it('ELECTRONIC markup on 100 should return 102', function() {
+	it('ELECTRONICS markup on 100 should return 2', function() {
   	});
 
-	it('PHARMACEUTICAL markup on 100 should return 107.5', function() {
+	it('DRUGS markup on 100 should return 7.5', function() {
   	});
 
-  	it('FOOD markup on 100 should return 113', function() {
+  	it('FOOD markup on 100 should return 13', function() {
   	});
 
-  	it('food (lower-cased) markup on 100 should return 113', function() {
+  	it('food (lower-cased) markup on 100 should return 13', function() {
   	});
 
-  	it('ALL OTHER TYPES markup on 100 should return 100', function() {
+  	it('ALL OTHER TYPES markup on 100 should return 0', function() {
   	});
 
-  	it('FOOD & PHARMACEUTICAL markups on 100 should return 120.5', function() {
+  	it('FOOD & DRUGS markups on 100 should return 20.5', function() {
+  	});
+});
+
+describe('Final cost estimate after applying markup(s) - success cases', function() {
+	it('Estimate for base price = 1299.99, Workers = 3 and Material = food should return 1,591.58', function() {
+  	});
+
+	it('Estimate for base price = $1299.99, Workers = 3 and Material = food should return $1,591.58', function() {
+  	});
+
+	it('Estimate for base price = $5,432.00, Workers = 1 and Material = drugs should return $6,199.81', function() {
+  	});
+
+  	it('Estimate for base price = $12,456.95, Workers = 4 and Material = books should return $1,591.58', function() {
+  	});
+
+  	it('Estimate for base price = $10,005.50, Workers = 2 and Material = food and drugs should return $12,911.60', function() {
   	});
 });
