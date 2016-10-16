868b9f89810d8e9a695c8c69ebc8469d464f5b66
Hassan Abbas <habbas99@gmail.com>
2016-10-16 16:33:18 -0400

    - currency convertor test cases

---
+++ tests/currency-test.js
@@ -0,0 +1,23 @@
+/**
+ * Test cases
+ *
+ */
+
+ var chai = require('chai');
+var expect = chai.expect;
+
+describe('Currency convertor - error cases', function() {
+	it('Amount must be a string', function() {
+  	});
+
+  	it('Cannot convert garbage amount to a number', function() {
+  	});
+});
+
+describe('Currency convertor - success cases', function() {
+	it('Amount 100.50 returns 100.50', function() {
+  	});
+
+  	it('Amount $1,009.50 should return 1009.50', function() {
+  	});
+});
